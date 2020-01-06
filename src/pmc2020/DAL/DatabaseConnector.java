692a224c94b8582b23b8a89fe53341fd29e46f48
dreamyyyyyy <31621059+dreamyy1@users.noreply.github.com>
2020-01-06 10:05:18 +0100

    Created Project

---
+++ src/pmc2020/DAL/DatabaseConnector.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pmc2020.DAL;
+
+/**
+ *
+ * @author CSnit
+ */
+public class DatabaseConnector
+{
+    
+}
